9c270c742ea2e8fda879de57cf44abc54512b0e4
CrozzDev <crozzdev95@outlook.com>
2024-01-01 23:37:45 -0500

    day 1 part 2 completed

--- Day1/day1_part2.py
+++ Day1/day1_part2.py
@@ -14,24 +14,66 @@ In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Addi
 
 What is the sum of all of the calibration values?"""
 
-def find_first(text_list):
+
+NUMS_DICT = {
+    "one": 1,
+    "two": 2,
+    "three": 3, 
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7, 
+    "eight": 8,
+    "nine": 9
+}
+
+def detect_number(text_list: str) -> str:
+    sub_word = ""
+    for char in text_list:
+        if char.isdigit():
+            return ""
+        sub_word += char
+        if sub_word in NUMS_DICT.keys():
+            return str(NUMS_DICT[sub_word])
+    return ""
+
+            
+            
+
+def find_first(text_list: str) -> str:
     first = ""
     if text_list == "":
         return ""
-    for char in text_list:
-        if char.isdigit():
-            first = str(char)
-            break
-        elif 
-    return first
+    
+    for i in range(len(text_list)):
+        if text_list[i].isdigit():
+            return str(text_list[i])
+        else:
+            first = detect_number(text_list[i:])
+            if first.isdigit():
+                return first
+            
+    return ""
 
 def find_last(text_list):
-    last= ""
-    if text_list == "":
-        return ""
-    reversed = text_list[::-1]
-    for char in reversed:
-        if char.isdigit():
-            last = str(char)
-            break
+    last = ""
+    for pos in range(len(text_list)):
+        digit = find_first(text_list[pos:])
+        if digit != "":
+            last = digit
     return last
+    
+
+def find_sum(input):
+    with open(input) as f:
+        lines = f.readlines()
+        sum = 0
+        for line in lines:
+            first = find_first(line)
+            last = find_last(line)
+            num = first + last
+            sum += int(num)
+    return sum
+            
+if __name__ == "__main__":
+    print(find_sum("input.txt"))
